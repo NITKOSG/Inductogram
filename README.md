e0fd0aba1e5a3dc30107c58722176787275007e7
Abhimanyu <abhimanyu305@gmail.com>
2018-11-17 23:30:43 +0530

    Added BoilerPlate

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Inductogram
+#Getting Started with Inductogram
+
+###This project is built in React.Js and NODE.js(v10.13.0)
+
+#Installation Guide
+1. Install [Node.Js](https://nodejs.org).
+2. change directory to Inductogram and run npm install.
+3. Crana would be installed along with the other dependencies and now run "crana dev" to start the project.
+
---
+++ src/server/index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const port = 5000;
+
+const { helloWorld } = require('@shared/util');
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end('Server started on port 5000', helloWorld('Server'));
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err)
+    return console.log('something bad happened', err);
+
+
+  console.log(`Server is listening on port ${port}`);
+});
